33d2c2168c45584f481dbb0b53112d7722fab33c
moyuweiqing <49243973+moyuweiqing@users.noreply.github.com>
2020-02-17 22:46:32 +0800

    Add files via upload

---
+++ 个股预测/multi-thread2_LSTM/n4.py
@@ -0,0 +1,130 @@
+import tushare as ts
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import os
+import xlrd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+import tensorflow as tf
+
+# 获取数据
+hs300 = ts.get_hs300s()
+suspend = [209, 290]
+choosed_stocks = []
+f = open('data.txt', 'w')
+
+my_df = pd.DataFrame(np.arange(0.0, 6000.0).reshape(300, 20),columns=range(0, 20))
+
+def getDayStock(day):
+    # my_df.to_csv('test.csv')
+    for i in range(110, 140):
+        if i in suspend:
+            continue
+        temp = ts.get_hist_data(hs300['code'].loc[i], start='2019-01-02', end='2019-09-30')
+        temp = temp.sort_index(ascending=True)
+        if(len(temp) != 183):
+            continue
+        # temp.to_csv('test.csv')
+        # print(temp.head())
+        new_data = pd.DataFrame(index=range(0, len(temp)), columns=['Date', 'Close'])
+        for j in range(0, len(temp)):
+            new_data['Date'][j] = temp.index[j]
+            new_data['Close'][j] = temp['close'][j]
+
+        new_data['Date'] = pd.to_datetime(new_data.Date, format='%Y-%m-%d')
+        new_data.index = new_data['Date']
+        print(new_data.iloc[162]) # 162是8.30
+
+        new_data.index = new_data.Date
+        new_data.drop('Date', axis=1, inplace=True)
+
+        # 创建训练集和验证集
+        dataset = new_data.values
+
+        train = dataset[0:163, :]  # 1-8月月训练集
+        valid = dataset[163:, :]  # 9月为测试集
+
+        # 将数据集转换为x_train和y_train
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaled_data = scaler.fit_transform(dataset)
+
+        x_train, y_train = [], []
+        for j in range(60, len(train)):
+            x_train.append(scaled_data[j - 60:j, 0])
+            y_train.append(scaled_data[j, 0])
+        x_train, y_train = np.array(x_train), np.array(y_train)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        # 创建和拟合LSTM网络
+        model = Sequential()
+        model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+        model.add(LSTM(units=50))
+        model.add(Dense(1))
+
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+
+        # 使用过去值来预测
+        inputs = new_data[len(new_data) - len(valid) - 60:].values
+        inputs = inputs.reshape(-1, 1)
+        inputs = scaler.transform(inputs)
+
+        X_test = []
+        for j in range(60, inputs.shape[0]):
+            X_test.append(inputs[j - 60:j, 0])
+        X_test = np.array(X_test)
+        # print(X_test)
+
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        closing_price = model.predict(X_test)
+        closing_price = scaler.inverse_transform(closing_price)
+
+        print(i)
+        train = new_data[:163]
+        valid = new_data[163:]
+        valid['Predictions'] = closing_price
+        for j in range(0, 20):
+            my_df.iloc[i][j] = valid['Predictions'].iloc[j]
+        my_df.to_csv('test4.csv')
+
+def sortAndChoose(dict, n): # dict是预测的股票收益率，n是股票个数
+    stocks = []
+    result = []
+    p = sorted([(k, v) for k, v in dict.items()], reverse=True)
+    # print(p)
+    s = set()
+    for i in p:
+        s.add(i[1])
+    for i in sorted(s, reverse=True)[:n]:
+        for j in p:
+            if j[1] == i:
+                result.append(j)
+    for r in result:
+        stocks.append(r[0])
+    print(stocks)
+    f.write(str(stocks)+'\n')
+    return stocks
+
+def count_yield(day):
+    day_yield = {}
+    for i in range(0, 300):
+        if day == 0:
+            temp = ts.get_hist_data(hs300['code'].loc[i], start='2019-01-02', end='2019-09-30')
+            temp = temp.sort_index(ascending=True)
+            a = my_df.iloc[i][0] / temp['close'].iloc[162]
+            day_yield[i] = a
+        else:
+            a = my_df.iloc[i][day] / my_df.iloc[i][day - 1]
+            day_yield[i] = a
+    b = sortAndChoose(day_yield, 10)
+    f.write(str(b)+'\n')
+    print(b)
+
+getDayStock(0)
+# for i in range(0, 20):
+#     count_yield(i)
+f.close()
---
+++ 个股预测/multi-thread_prophet/m11.py
@@ -0,0 +1,82 @@
+import tushare as ts
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import os
+import xlrd
+from fbprophet import Prophet
+import pystan
+
+hs300 = ts.get_hs300s()
+suspend = [209, 290]
+day_yield = {}
+choosed_stocks = []
+f = open('data11.txt', 'w')
+
+def make_prediction(training_set, validation_set):
+    model = Prophet()
+    model.fit(training_set)
+
+    # 预测
+    close_prices = model.make_future_dataframe(periods=len(validation_set))
+    forecast = model.predict(close_prices)
+    forecast_valid = forecast[-1:]
+    # print(forecast)
+    last_day = forecast[-2:-1]
+    #print(last_day)
+    #print(forecast_valid)
+    # a = forecast_valid['yhat'][1] - last_day['yhat'][1]
+    y= (forecast.iat[-1, -1] - forecast.iat[-2, -1]) / forecast.iat[-2, -1]
+    # print(y)
+    return y
+    #forecast_valid = forecast['yhat'][402:]
+
+def getDayStock(day):
+    for i in range(0, 300):
+        if i in suspend:
+            continue
+        temp = ts.get_hist_data(hs300['code'].loc[i], start='2019-01-02', end='2019-09-30')
+        temp = temp.sort_index(ascending=True)
+        # print(temp.head())
+        new_data = pd.DataFrame(index=range(0, len(temp)), columns=['Date', 'Close'])
+        for j in range(0, len(temp)):
+            new_data['Date'][j] = temp.index[j]
+            new_data['Close'][j] = temp['close'][j]
+
+        new_data['Date'] = pd.to_datetime(new_data.Date, format='%Y-%m-%d')
+        new_data.index = new_data['Date']
+
+        # 准备数据
+        new_data.rename(columns={'Close': 'y', 'Date': 'ds'}, inplace=True)
+        row = temp.iloc[:, 0].size
+        # print(temp.head())
+        # temp.to_csv('test.csv')
+        training_number = row - 20 + day
+        training_set = new_data[: training_number]
+        validation_set = new_data[training_number:training_number + 1]
+        prediction = make_prediction(training_set, validation_set)
+        day_yield[i] = prediction
+
+def sortAndChoose(dict, n): # dict是预测的股票和收益率，n是股票个数
+    stocks = []
+    result = []
+    p = sorted([(k, v) for k, v in dict.items()], reverse=True)
+    # print(p)
+    s = set()
+    for i in p:
+        s.add(i[1])
+    for i in sorted(s, reverse=True)[:n]:
+        for j in p:
+            if j[1] == i:
+                result.append(j)
+    for r in result:
+        stocks.append(r[0])
+    print(stocks)
+    f.write(str(stocks)+'\n')
+    return stocks
+
+getDayStock(10)
+print(day_yield)
+choosed_stocks = sortAndChoose(day_yield, 10)
